bc98bab12e389ded431cfbd432d0d6bf882610d1
Sevil2005 <sevillerasulova@gmail.com>
2020-12-03 02:44:04 +0400

    admin edits are ready

--- admin/routes.py
+++ admin/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import User, Post, book, category
-from flask import render_template, url_for, redirect, abort
+from flask import render_template, url_for, redirect, request, abort
 from app.forms import BookForm, CatForm
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -64,6 +64,31 @@ def bookAddForm():
     else:
         abort(403)
 
+@app.route('/admin/kitablar/kitab-redaktə-et/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def edit_book(id):
+    if User.query.get(1) == current_user:
+        selectedBook = book.query.get_or_404(id)
+        categories = category.query.all()
+        form = BookForm()
+        if form.validate_on_submit():
+            cat = form.category.data
+            newCatObj = category.query.filter_by(id = cat).first()
+            selectedBook.name = form.name.data
+            selectedBook.description = form.description.data
+            selectedBook.url = form.url.data
+            selectedBook.category = newCatObj
+            db.session.commit()
+            return redirect(url_for('bookTable'))
+        elif request.method =='GET':
+            form.name.data = selectedBook.name
+            form.description.data = selectedBook.description
+            form.url.data = selectedBook.url
+            form.category.data = selectedBook.category.id
+        return render_template('/admin/bookAddForm.html', categories=categories, form=form)
+    else:
+        abort(403)
+
 @app.route('/admin/kitablar/kitab-sil/<int:id>', methods = ['GET'])
 @login_required
 def delete_book(id):
@@ -74,7 +99,7 @@ def delete_book(id):
         return redirect(url_for("bookTable"))
     else:
         abort(403)
-        
+
 #Categories
 
 @app.route('/kateqoriya-əlavə-et/yeni', methods=['GET', 'POST'])
@@ -91,6 +116,22 @@ def addCategory():
     else:
         abort(403)
 
+@app.route('/admin/kitablar/kateqoriya-redaktə-et/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def edit_cat(id):
+    if User.query.get(1) == current_user:
+        selectedCat = category.query.get_or_404(id)
+        form = CatForm()
+        if form.validate_on_submit():
+            selectedCat.name = form.name.data
+            db.session.commit()
+            return redirect(url_for('bookTable'))
+        elif request.method =='GET':
+            form.name.data = selectedCat.name
+        return render_template('/admin/addCategory.html', form=form)
+    else:
+        abort(403)
+
 @app.route('/admin/kitablar/kateqoriya-sil/<int:id>', methods = ['GET'])
 @login_required
 def delete_cat(id):
@@ -102,6 +143,7 @@ def delete_cat(id):
     else:
         abort(403)
 
+# Advice Blog
 
 @app.route('/advice-articles')
 @login_required
